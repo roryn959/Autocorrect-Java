5328f7221a446a4507b5cec8521a2838edc83e7f
roryn959 <zcabrbn@ucl.ac.uk>
2021-01-25 15:26:33 +0000

    Starting StringArray object and creating a testing class.

--- src/StringArray.java
+++ src/StringArray.java
@@ -1,2 +1,16 @@
+//Written by Rory Nicholas 25/01/2021
+//StringArray class to hold and manipulate an array of Strings.
+
 public class StringArray {
+    private String[] array;
+
+    public StringArray(){
+        this.array = new String[1];
+    }
+    public String[] getArray(){
+        return array;
+    }
+    public StringArray(StringArray a){
+        this.array = a.getArray();
+    }
 }
