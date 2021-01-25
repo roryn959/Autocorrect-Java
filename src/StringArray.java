6f6cc307b3152253450cfe63a804b3333165175a
roryn959 <zcabrbn@ucl.ac.uk>
2021-01-25 17:17:26 +0000

    Added some methods. Up to set.

--- src/StringArray.java
+++ src/StringArray.java
@@ -2,15 +2,64 @@
 //StringArray class to hold and manipulate an array of Strings.
 
 public class StringArray {
-    private String[] array;
+    private String[] array; //The array to hold the strings.
+    //Number of items in array and array size limit tracked to allow array to be rebalanced.
+    private int numElements; //Number of items.
+    private int numLimit; //Current array size limit.
 
+    //Constructor with no argument.
     public StringArray(){
         this.array = new String[1];
+        this.numElements = 0;
+        this.numLimit = 1;
     }
+
+    public void display(){
+        for (int i=0; i<this.numElements; i++){
+            System.out.println(this.array[i]);
+        }
+    }
+
+    //Getters
     public String[] getArray(){
-        return array;
+        return array.clone(); //Return a copy of the array to prevent user from modifying array directly and/or unintentionally
     }
+    public int getNumElements(){
+        return this.numElements;
+    }
+    public int getNumLimit(){
+        return this.numLimit;
+    }
+
+    //Constructor to copy a StringArray argument.
     public StringArray(StringArray a){
         this.array = a.getArray();
+        this.numElements = a.getNumElements();
+        this.numLimit = a.getNumLimit();
+    }
+
+    public int size(){
+        return this.numElements;
+    }
+
+    public boolean isEmpty(){
+        return (this.numElements == 0);
+    }
+
+    public String get(int index){
+        if (index<0 || index>=this.getNumElements()){ //If index given is negative or outside of the range of elements in array, it's invalid.
+            return null;
+        }
+        else{
+            return this.array[index];
+        }
+    }
+
+    public void set(int index, String s){
+        if (index>=0 && index<this.getNumElements()) { //If index given is between 0 and (length of the list - 1) inclusive it's valid
+            this.array[index] = s;
+            this.numElements++;
+        }
+
     }
 }
--- src/Tester.java
+++ src/Tester.java
@@ -1,5 +1,13 @@
 public class Tester {
     public static void main(String[] args) {
         StringArray myArray = new StringArray();
+        myArray.display();
+        myArray.set(0, "Hello");
+        myArray.display();
+        myArray.set(1, "my");
+        myArray.set(2, "name is rozza");
+        myArray.display();
+        System.out.println(myArray.getNumElements());
+        System.out.println(myArray.getNumLimit());
     }
 }
