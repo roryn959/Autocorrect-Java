67e9610dd5d1e19ae82bd8b53887f0667d026358
roryn959 <zcabrbn@ucl.ac.uk>
2021-01-25 15:18:07 +0000

    Resetting project to start from scratch.

--- src/StringArray.java
+++ src/StringArray.java
@@ -1,7 +1,2 @@
 public class StringArray {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("This is my new commit!");
-        System.out.println("And this is testing whether I can push to remote origin :)");
-    }
 }
