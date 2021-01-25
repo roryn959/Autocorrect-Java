ee0e0820ffa513543020a8b7f8a8052b5e83fde0
roryn959 <zcabrbn@ucl.ac.uk>
2021-01-25 15:12:11 +0000

    Testing commits.

--- src/StringArray.java
+++ src/StringArray.java
@@ -1,5 +1,6 @@
 public class StringArray {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("This is my new commit!");
     }
 }
